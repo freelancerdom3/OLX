b4d8afde8853283f655f347b59d5d5091ff94bca
Rushi Rajwade <rushi.rajwade@promptsoftech.in>
2023-10-03 19:05:02 +0530

    Login authentication added

--- OLX_MVC/Olx_New_Project/Models/DataAccess.cs
+++ OLX_MVC/Olx_New_Project/Models/DataAccess.cs
@@ -21,6 +21,67 @@ namespace Olx_New_Project.Models
         }
 
 
+        public bool IsAdmin(string userEmail)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            string query = "select roles from users where userEmail=@userEmail and Roles='Admin'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userEmail",userEmail);
+            conn.Open();
+            string role = (string)cmd.ExecuteScalar();
+            conn.Close();
+            return role == "Admin";
+        }
+
+        public bool authLogin(UserModel model, out string validationmsg)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            string q = "select count(*) from Users where userEmail=@userEmail";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            cmd.Parameters.AddWithValue("@userEmail", model.userEmail);
+
+            conn.Open();
+            int count = (int)cmd.ExecuteScalar();
+            conn.Close();
+            if (count == 0)
+            {
+                validationmsg = "UserEmail doesn't Exist";
+                return false;
+            }
+
+
+
+
+            string query = "select count(*) from Users where userEmail=@userEmail and userPassword=@userPassword";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userEmail", model.userEmail);
+            cmd.Parameters.AddWithValue("@userPassword", model.userPassword);
+            conn.Open();
+            count = (int)cmd.ExecuteScalar();
+            conn.Close();
+            validationmsg = count == 0 ? "invalid credintials" : string.Empty;
+            if (count > 0)
+            {
+                bool isadmin = IsAdmin(model.userEmail);
+                if (isadmin)
+                {
+                    return true;
+                }
+            }
+            return count > 0;
+            //if (rows == 0)
+            //{  
+
+
+            //}
+            ////return false;
+            //validationmsg = "Invalid Credentials ";
+            //return rows>0 ;
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dataTable = new DataTable();
+
+        }
+
         public IEnumerable<ProductSubCategoryModel> GetProductDetailsLists()
         {
             connection();
